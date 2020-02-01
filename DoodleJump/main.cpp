268d2f207713227fd1adafa04a86c999ee0090d5
Николаев Виталий <vs.nikolaev.bmstu@gmail.com>
2020-02-02 01:07:52 +0300

    Начато переписывание в ООП

--- DoodleJump/main.cpp
+++ DoodleJump/main.cpp
@@ -6,6 +6,60 @@
 #include <time.h>
 using namespace sf;
 
+const int WindowWidth = 400;
+const int WindowHeight = 533;
+const int DoodlerWidth = 60;
+
+class Doodler
+{
+public:
+	Doodler();
+	Doodler(int x, int y);
+	~Doodler();
+	const int Width = 60;
+	void TurnRight();
+	void TurnLeft();
+
+private:
+	int x, y;
+	Sprite m_Sprite;
+	Texture TextureDoodleR;
+	Texture TextureDoodleL;
+
+};
+
+Doodler::Doodler()
+{
+	x = 0;
+	y = 0;
+	TextureDoodleR.loadFromImage(LoadResourcePicturePNG(IDB_PNG2));
+	TextureDoodleL.loadFromImage(LoadResourcePicturePNG(IDB_PNG3));
+	m_Sprite.setTexture(TextureDoodleR);
+}
+
+Doodler::Doodler(int x, int y)
+{
+	Doodler::x = x;
+	Doodler::y = x;
+	TextureDoodleR.loadFromImage(LoadResourcePicturePNG(IDB_PNG2));
+	TextureDoodleL.loadFromImage(LoadResourcePicturePNG(IDB_PNG3));
+	m_Sprite.setTexture(TextureDoodleR);
+}
+
+Doodler::~Doodler()
+{
+}
+
+void Doodler::TurnRight()
+{
+	m_Sprite.setTexture(TextureDoodleR);
+}
+
+void Doodler::TurnLeft()
+{
+	m_Sprite.setTexture(TextureDoodleL);
+}
+
 struct point
 {
 	int x, y;
@@ -41,11 +95,20 @@ Font LoadResourceFont(int FontResourceCode)
 	return ReturnFont;
 }
 
+void SetPlatPosition(std::vector<point> &Plats)
+{
+	for (int i = 0; i < Plats.size(); i++)
+	{
+		Plats[i].x = (rand() % WindowWidth) - 40; // -40 чтобы не выступала слишком далеко за край экрана
+		Plats[i].y = rand() % WindowHeight;
+	}
+}
+
 int main()
 {
 	srand(time(0));
 
-	RenderWindow app(VideoMode(400, 533), "Doodle Game!");
+	RenderWindow app(VideoMode(WindowWidth, WindowHeight), "Doodle Game!");
 	app.setFramerateLimit(60);
 
 	// Загрузка текстур
@@ -71,16 +134,20 @@ int main()
 	AltitudeText.setFillColor(Color(80, 200, 30, 255));
 	AltitudeText.setOutlineColor(Color(0, 0, 0, 255));
 	AltitudeText.setOutlineThickness(2);
+	AltitudeText.setPosition(350, 10);
 	
-	Sprite sBackground(TextureBackground), sPlat(TextureRegularPlat), sPers(TextureDoodleR);
+	// Подготовка спрайтов фона, платформ и дудлера
+	Sprite sBackground(TextureBackground), sPlat(TextureRegularPlat), sPers(TextureDoodleR), sPersMirror(TextureDoodleR);
 
-	point plat[20];
-
-	for (int i = 0; i < 10; i++)
+	std::vector<point> Plats;
+	for (unsigned i = 0; i < 10; i++)
 	{
-		plat[i].x = rand() % 400;
-		plat[i].y = rand() % 533;
+		point newPlat;
+		Plats.push_back(newPlat);
 	}
+
+
+	SetPlatPosition(Plats);
 
 	int x = 100, y = 100, h = 200;
 	float dx = 0, dy = 0;
@@ -116,13 +183,13 @@ int main()
 			if (Keyboard::isKeyPressed(Keyboard::Right))
 			{
 				x += 3;
-				if (x >= 400) x = 0;
+				if (x >= WindowWidth) x = 0;
 				sPers.setTexture(TextureDoodleR);
 			}
 			if (Keyboard::isKeyPressed(Keyboard::Left))
 			{
 				x -= 3;
-				if (x < 0) x = 400;
+				if (x < 0) x = WindowWidth;
 				sPers.setTexture(TextureDoodleL);
 			}
 
@@ -131,49 +198,56 @@ int main()
 			y += dy;
 			
 			// Конец игры при падении
-			if (y > 500) GameOverFlag = true;
+			if (y > WindowHeight) GameOverFlag = true;
 
-			// Если персонаж поднялся достаточно высоко (выше отметки [533 - h]), сместить платформы вниз
+			// Если персонаж поднялся достаточно высоко (выше отметки [WindowHeight - h]), сместить платформы вниз
 			if (y < h)
 			{
 				for (int i = 0; i < 10; i++)
 				{
 					y = h;
-					plat[i].y = plat[i].y - dy;
+					Plats[i].y -= dy;
 
 					//Если платформа опустилась ниже границы экрана, создать новую платформу сверху
-					if (plat[i].y > 533)
+					if (Plats[i].y > WindowHeight)
 					{
-						plat[i].y = 0;
-						plat[i].x = rand() % 400;
+						Plats[i].x = (rand() % WindowWidth) - 40; // -40 чтобы не выступала слишком далеко за край экрана
+						Plats[i].y = 0;
 					}
 				}
 
 				AltitudeCount -= dy; //Отнимаем величину подъёма, т.к. система координат направлена вниз
 			}
 				
-
 			// Отскок от платформы
 			for (int i = 0; i < 10; i++)
 			{
-				if ((x + 50 > plat[i].x) && (x + 20 < plat[i].x + 68) && (y + 70 > plat[i].y) && (y + 70 < plat[i].y + 14) && (dy > 0)) dy = -10;
+				if ((x + 50 > Plats[i].x) && (x + 20 < Plats[i].x + 68) && (y + 65 > Plats[i].y) && (y + 65 < Plats[i].y + 14) && (dy > 0)) dy = -10;
 			}
 		}
 
 		// Начало отрисовка кадра
 		sPers.setPosition(x, y);
-		AltitudeText.setPosition(350, 10);
 		AltitudeText.setString(std::to_string((int)round(AltitudeCount/10)));
 
 		app.draw(sBackground);
 		
 		for (int i = 0; i < 10; i++)
 		{
-			sPlat.setPosition(plat[i].x, plat[i].y);
+			sPlat.setPosition(Plats[i].x, Plats[i].y);
 			app.draw(sPlat);
 		}
 
 		app.draw(sPers);
+		if (WindowWidth - x < DoodlerWidth)
+		{
+			Texture texture;
+			texture = *sPers.getTexture();
+			sPersMirror.setTexture(texture);
+			sPersMirror.setPosition(-(WindowWidth - x), y);
+			app.draw(sPersMirror);
+		}		
+
 		app.draw(AltitudeText);
 
 		if (GameOverFlag) app.draw(GameOverText);
